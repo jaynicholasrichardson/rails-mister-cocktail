920657e54d4611b432dfe8433e970c117191921d
Jay Richardson <super.jay21@gmail.com>
2020-08-14 11:25:08 +0100

    committing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles, except: :index
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_130141) do
+ActiveRecord::Schema.define(version: 2020_08_14_095627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
